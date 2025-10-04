db9f92c01d19916e05830de5edbd976d821defbe
Maheboob basha <skmaheboobbasha1306@gmail.com>
2025-10-04 15:50:53 +0530

    ALERTS CHANGES

--- alerts.java
+++ alerts.java
@@ -1,5 +1,26 @@
 package SELENIUMTESTS;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class alerts {
+	@Test
+	public  void aleertsandhandles() {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver bash=new ChromeDriver();
+		bash.get("https://www.facebook.com/");
+		bash.findElement(By.xpath(""));
+		
+		
+		
+		
+		
+	}
 
 }
